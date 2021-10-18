115751e0717dff864ed29e4ffeb56c4bf1760bae
Hasibur <hasibur01631753881@gmail.com>
2021-10-18 18:24:14 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Navbar></Navbar>
+      <Switch>
+        <Route>
+          
+        </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
